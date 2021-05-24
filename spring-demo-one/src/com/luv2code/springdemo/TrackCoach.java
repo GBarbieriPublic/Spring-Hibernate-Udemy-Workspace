f27e27064fa4c201e655c479db19199b9d4230db
Giuseppe Barbieri <kinggiuseppe1@gmail.com>
2021-05-24 01:08:16 -0400

    Section 6: Bean Lifecycle
    Completed section where we went over the bean init-method and destroy-method.
    Using these we can run custom code during bean initialization and bean destruction.

--- spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -4,8 +4,8 @@ public class TrackCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
-	
 	public TrackCoach() {
+		
 	}
 
 	public TrackCoach(FortuneService fortuneService) {
@@ -22,4 +22,13 @@ public class TrackCoach implements Coach {
 		return "You have the Track Coach Blessing: " + fortuneService.getFortune();
 	}
 
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyCleanupStuff() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuff");
+	}
 }
